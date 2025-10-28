cb51ce206f32f3b74706f1d0aa1e59a483dbe257
Atharva <athrudev@gmail.com>
2025-10-28 08:28:10 +0530

    Solved the Bellman ford program

---
+++ bf.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<limits.h>
+#include<vector>
+using namespace std;
+
+ struct Edge{
+        int u,v,w;
+    };
+
+
+int main(){
+
+    int V,E,source;
+    cout << "Enter the number of vettices: ";
+    cin>>V;
+
+    cout<<"Enter the number of Edges: ";
+    cin>>E;
+    
+    vector<Edge> edge(E);
+    cout<<"\nEnter the input in the format (u v w):\n";
+    for(int i=0;i<E;i++){
+        cin>>edge[i].u>>edge[i].v>>edge[i].w;
+    }
+
+    cout<<"\nEnter the source Vertex";
+    cin>>source;
+
+    vector<int> dist(V,INT_MAX);
+    vector<int> Parent(V,-1);
+
+    for(int i=0;i<V-1;i++){
+        for(int j=0;j<E;j++){
+            int u=edge[i].u;
+            int v=edge[i].v;
+            int w=edge[i].w;
+            if(dist[u]!=INT_MAX || (dist[u]+w) < dist[v] ){
+                dist[v]=dist[u]+w;
+                Parent[v]=u;
+            }
+        }
+    }
+
+
+    bool isCycle=false;
+    for(int i=0;i<V-1;i++){
+        for(int j=0;j<E;j++){
+            int u=edge[i].u;
+            int v=edge[i].v;
+            int w=edge[i].w;
+            if(dist[u]!=INT_MAX || (dist[u]+w)<dist[v]){
+                isCycle=true;
+            }
+        }
+    }
+
+    if(isCycle){
+        cout<<"There is a negative Edge cycle in the graph";
+    }else {
+        cout << "\nVertex\tDistance\tParent" << endl;
+        for (int i = 0; i < V; i++) {
+            cout << i << "\t";
+            if (dist[i] == INT_MAX) 
+                cout << "INF\t\t";
+            else
+                cout << dist[i] << "\t\t";
+            
+            if (Parent[i] == -1)
+                cout << "None";
+            else
+                cout << Parent[i];
+            cout << endl;
+        }
+    }
+
+    return 0;
+
+}
